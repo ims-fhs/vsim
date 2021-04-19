731a23fab29d001fe50d07a7adfa41239f1712ad
Michael Schmid <michi.schmid@ost.ch>
2021-04-19 15:37:42 +0200

    Swtich App back from "Kinderbetreuung" to "Aeltere Arbeitnehmende", because here we already have testcases in data/outputs/

--- inst/shiny-apps/vsim/R/import.R
+++ inst/shiny-apps/vsim/R/import.R
@@ -16,10 +16,10 @@
 #' Q2a <- import_from_excel(give_me = "Q2a")
 #' Q2b <- vsim:::import_from_excel(give_me = "Q2b")
 #' Q2c <- import_from_excel(give_me = "Q2c")}
-# import_from_excel <- function(path = "data/vsim_question_lists_aeltere_arbeitnehmende.xlsx",
-#                               give_me = c("QAG1", "Q1", "A1", "G1", "Gtype", "Q2a", "Q2b", "Q2c")) {
-import_from_excel <- function(path = "data/vsim_question_lists_kinderbetreuung.xlsx",
+import_from_excel <- function(path = "data/vsim_question_lists_aeltere_arbeitnehmende.xlsx",
                               give_me = c("QAG1", "Q1", "A1", "G1", "Gtype", "Q2a", "Q2b", "Q2c")) {
+# import_from_excel <- function(path = "data/vsim_question_lists_kinderbetreuung.xlsx",
+#                               give_me = c("QAG1", "Q1", "A1", "G1", "Gtype", "Q2a", "Q2b", "Q2c")) {
   assertthat::assert_that(is.character(give_me))
   assertthat::assert_that(length(give_me) == 1)
   assertthat::assert_that(give_me %in% c("QAG1", "Q1", "A1", "G1", "Gtype", "Q2a", "Q2b", "Q2c"))
@@ -57,8 +57,8 @@ import_from_excel <- function(path = "data/vsim_question_lists_kinderbetreuung.x
 #' @param path a character
 export_to_excel <- function(Alist_Answer, path = "data/outputs/") {
   # standard path is set to "data/outputs/" because the function is called from within render_main_panel_1.R
-  # empty_list <- import_from_excel(path = "data/vsim_question_lists_aeltere_arbeitnehmende.xlsx", give_me = "QAG1")
-  empty_list <- import_from_excel(path = "data/vsim_question_lists_kinderbetreuung.xlsx", give_me = "QAG1")
+  empty_list <- import_from_excel(path = "data/vsim_question_lists_aeltere_arbeitnehmende.xlsx", give_me = "QAG1")
+  # empty_list <- import_from_excel(path = "data/vsim_question_lists_kinderbetreuung.xlsx", give_me = "QAG1")
   empty_list$Answer_given <- Alist_Answer
   # path_to_file <- paste0(path, format(Sys.time(), "%Y_%m_%d"),"_Answer_list_",
   #                        format(Sys.time(), "%H_%M_%S"),".xlsx")
